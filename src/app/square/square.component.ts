258c602c9b60507802454784a0b8350022218a65
Ludmi Lopez <ludmit@outlook.com>
2022-08-25 18:29:45 -0300

    update

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -4,13 +4,21 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   styleUrls:['./square.component.scss'],
   template: `
-<button>{{ value }}</button>
+<button nbButton *ngIf="!value">{{ value }}</button>
+<button nbButton *ngIf="value === 'X'">{{ value }}</button>
+<button [ngClass]="statusClass" (click)="setActiveClass()" nbButton *ngIf="value === 'O'">{{ value }}</button>
   `,
-  styles: []
+  styles: ['']
 })
 export class SquareComponent {
 
   @Input() value: 'X' | 'O';
+
+  statusClass = 'not-active';
+
+  setActiveClass(){
+      this.statusClass = 'active';
+  }
 
 }
 
